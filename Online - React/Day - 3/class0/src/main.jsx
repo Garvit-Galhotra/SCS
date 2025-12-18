4598ef3f798d5e0850f889d26b886291437edd7b
Garvit-Galhotra <garvitgalhotra1905@gmail.com>
2025-12-18 21:49:49 +0530

    Day-3 Understanding React+Vite

---
+++ Online - React/Day - 3/class0/src/main.jsx
@@ -0,0 +1,9 @@
+
+// import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App/>);
