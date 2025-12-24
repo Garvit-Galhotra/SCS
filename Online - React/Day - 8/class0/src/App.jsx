c6aa248c58130866d505374954a918e22b4fa176
Garvit-Galhotra <garvitgalhotra1905@gmail.com>
2025-12-24 22:16:13 +0530

    Day-8

---
+++ Online - React/Day - 8/class0/src/App.jsx
@@ -0,0 +1,96 @@
+// // import React, { useState } from "react";
+
+// const App = () => {
+//   // const [King, setKing] = useState("Krishna");
+//   // const [Queen, setQueen] = useState("Radha");
+//   // let a = 0;
+
+//   // const add = () => {
+//   //   console.log("btn clicked");
+//   //   setKing("Papa");
+//   //   setQueen("Mummy");
+//   // };
+
+//   // const ChangeQueen = () => {
+//   //   setQueen("Mummy");
+//   // };
+
+//   // const ChangeKing = () => {
+//   //   setKing("Papa");
+//   // };
+
+//   // const ChangeBoth = () => {
+//   //   setQueen("Mummy");
+//   //   setKing("Papa");
+//   // };
+
+//   // const Reset = () => {
+//   //   setKing("Krishna");
+//   //   setQueen("Radha");
+//   // };
+
+//   return (
+//     <div>
+//       {/* <h1>{Queen}</h1>
+//       <h2>{King}</h2>
+//       <button onClick={ChangeQueen}>ChangeQueen</button>
+//       <button onClick={ChangeKing}>ChangeKing</button>
+//       <button onClick={ChangeBoth}>ChangeBoth</button>
+//       <button onClick={Reset}>Reset</button> */}
+
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import React, { useState } from "react";
+
+// const App = () => {
+//   const [num, setnum] = useState(0);
+
+//   return (
+//     <div>
+//       <h1>{num}</h1>
+//       <button
+//         onClick={() => {
+//           setnum(num + 1);
+//         }}
+//       >
+//         Increase
+//       </button>
+//       <button
+//         onClick={() => {
+//           setnum(num - 1);
+//         }}
+//       >
+//         Decrease
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import React, { useState } from "react";
+
+// const App = () => {
+//   let [num, setNum] = useState(0);
+
+//   return (
+//     <div>
+//       <h1>{num}</h1>
+//       <button onClick={() => setNum(num++)}>Change</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
