253b500f76f3661a5ec331dcf6b5239e574eb5b5
Garvit-Galhotra <garvitgalhotra1905@gmail.com>
2026-02-10 22:34:28 +0530

    Project Day - 12 Part -  1

---
+++ Backend/Day - 12 Project/src/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: [true, "User name already exist"],
+    required: [true, "User name is required"],
+  },
+  email: {
+    type: String,
+    unique: [true, "User name already exist"],
+    required: [true, "Email is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  bio: String,
+  profile_image: {
+    type: String,
+    default:
+      "https://ik.imagekit.io/cx025vvzg/default-avatar-profile-icon-vector-social-media-user-image-182145777.webp",
+  },
+});
+
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
---
+++ Backend/Day - 12 Project/src/routes/auth.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const authController = require("../controllers/auth.controller");
+
+
+const authrouter = express.Router();
+
+
+authrouter.post("/register", authController.registerController);
+
+authrouter.post("/login", authController.loginController);
+
+module.exports = authrouter;
