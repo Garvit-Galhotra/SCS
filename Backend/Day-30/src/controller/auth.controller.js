cdc3bcd69bcfb8ce10b7044f613148fc06bac642
Garvit-Galhotra <garvitgalhotra1905@gmail.com>
2026-03-07 22:03:22 +0530

    Day - 30

---
+++ Backend/Day-30/src/controller/auth.controller.js
@@ -0,0 +1,14 @@
+export async function registerUser(req, res, next) {
+  res.status(201).json({
+    message: "User registered successfully",
+  });
+}
+
+/**
+ * user =>
+ * {
+ * username:{ type: String, required: true },
+ * email:{ type: String, required: true,unique: true },
+ * password:{ type: String, required: true }
+ * }
+ */
